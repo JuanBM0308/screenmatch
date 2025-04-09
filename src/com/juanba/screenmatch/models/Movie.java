1d2a6fe30fadf51290608805dc1bf184c142a10b
JuanBarbosaMora <portelnom0308@gmail.com>
2025-04-09 16:44:29 -0500

    add kwonledge this, private, public

---
+++ src/com/juanba/screenmatch/models/Movie.java
@@ -0,0 +1,63 @@
+package com.juanba.screenmatch.models;
+
+public class Movie {
+    private String name;
+    private int launchDate;
+    private int durationInMinutes;
+    private boolean includedInThePlan;
+    private double sumOfTheEvaluations;
+    private int totalOfTheEvaluation;
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(int launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public boolean isIncludedInThePlan() {
+        return includedInThePlan;
+    }
+
+    public void setIncludedInThePlan(boolean includedInThePlan) {
+        this.includedInThePlan = includedInThePlan;
+    }
+
+    public int getTotalOfTheEvaluation() {
+        return totalOfTheEvaluation;
+    }
+
+    public void evaluates(double note) {
+        sumOfTheEvaluations += note;
+        totalOfTheEvaluation++;
+    }
+
+    public double calculateAverage() {
+        return sumOfTheEvaluations / totalOfTheEvaluation;
+    }
+
+    public void showTechnicalSheet() {
+        System.out.println("Pelicula: " + name);
+        System.out.println("Fecha de lanzamiento: " + launchDate);
+        System.out.println("Duración de la pelicula: " + durationInMinutes + " minutos");
+        System.out.println("Esta incluida en tu plan: " + includedInThePlan);
+    }
+}
